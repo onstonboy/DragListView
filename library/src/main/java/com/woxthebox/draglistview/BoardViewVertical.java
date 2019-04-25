0d065f13b0227b4e4b7de7175729ecb1053b0efd
Lê Văn Chương <vanchuong119@gmail.com>
2019-04-25 09:03:22 +0700

    Optimize part 2

--- library/src/main/java/com/woxthebox/draglistview/BoardViewVertical.java
+++ library/src/main/java/com/woxthebox/draglistview/BoardViewVertical.java
@@ -674,9 +674,9 @@ public class BoardViewVertical extends LinearLayout implements AutoScroller.Auto
         }
     }
 
-    public void itemDraggingChanged() {
-        mItemDraggingChanged = true;
-        mCurrentRecyclerView.itemDraggingChanged();
+    public void itemDraggingChanged(boolean isItemDraggingChange) {
+        mItemDraggingChanged = isItemDraggingChange;
+        mCurrentRecyclerView.itemDraggingChanged(isItemDraggingChange);
     }
 
     public boolean isDragEnabled() {
